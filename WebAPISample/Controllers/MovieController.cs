dcea4ff5d026586b39551cc053089be7269f33a3
Adam Nobs <nobsadam@gmail.com>
2020-01-13 14:10:05 -0600

    Added a migration and starter working on the Gets for the API

--- WebAPISample/Controllers/MovieController.cs
+++ WebAPISample/Controllers/MovieController.cs
@@ -10,23 +10,34 @@ namespace WebAPISample.Controllers
 {
     public class MovieController : ApiController
     {
+        
+        private ApplicationDbContext db = new ApplicationDbContext();
         // GET api/values
-        public IEnumerable<string> Get()
+        [HttpGet]
+        public IHttpActionResult Get()
         {
-            // Retrieve all movies from db logic
-            return new string[] { "movie1 string", "movie2 string" };
+            var movies = db.Movies;   // Retrieve all movies from db logic
+            return Ok(movies);
         }
 
         // GET api/values/5
-        public string Get(int id)
+        [HttpGet]
+        public IHttpActionResult Get(int id)
         {
+            var movie = db.Movies.Find(id);
             // Retrieve movie by id from db logic
-            return "value";
+            return Ok(movie);
         }
 
         // POST api/values
         public void Post([FromBody]Movie value)
         {
+            Movie movie = new Movie();
+            movie.Title = value.Title;
+            movie.Genre = value.Title;
+            movie.Director = value.Director;
+            db.SaveChanges();
+            
             // Create movie in db logic
         }
 
@@ -39,6 +50,8 @@ namespace WebAPISample.Controllers
         // DELETE api/values/5
         public void Delete(int id)
         {
+            Movie movie = db.Movies.Find(id);
+
             // Delete movie from db logic
         }
     }
